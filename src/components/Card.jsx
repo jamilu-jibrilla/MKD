e3fc89ee8e204de89255e4008ce1971958ca9773
jamil <jamilujibrilla01@gmail.com>
2023-02-04 13:32:31 +0100

    style: updated styles for dashboard page

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import {useDrag} from "react-dnd"
+import likes from "./likes.png"
 
 const Card = ({card, handleDrag}) => {
 
@@ -19,16 +20,17 @@ const Card = ({card, handleDrag}) => {
             }}
             id={card.id}
             onDragEnter={handleDrag}
-                className=" flex justify-between items-center  text-[#FFFFFF] mt-5 ">
+                className=" flex justify-between items-center border border-[#FFFFFF1F] px-3 rounded-[16px] text-[#666666] mt-5 ">
           <div className="flex items-center w-[30%] ">
-            <h4 className="mr-5 opacity-[0.7]">#</h4>
+            <h4 className="mr-5 ">#</h4>
             <div className="h-[90px] w-[200px] overflow-hidden  mr-3 mt-3 rounded-[4px] ">
-              <img src={card.photo ? card.photo :"https://picsum.photos/seed/picsum/200/200"} className="h-[auto] w-[auto]" />
+              <img src={card.photo ? card.photo :"https://picsum.photos/seed/picsum/200/200"} className="h-[70px] w-[200px]" />
             </div>
-            <h4 className="opacity-[0.7]">{card.title}</h4>
+            <h4 className="">{card.title}</h4>
           </div>
-          <h4 className="w-[29%] opacity-[0.7]">mananight</h4>    
-          <h4 className="opacity-[0.7]">Most Liked</h4>    
+          <h4 className="w-[35%] opacity-[0.5] text-[16px] text-[#DBFD51]">mananight</h4>    
+          
+          <h4 className="flex items-center mr-3">{card.like} <img className="ml-1" src={likes} alt="" /></h4>    
           </div>
     )
 }
--- src/pages/AdminDashboardPage.jsx
+++ src/pages/AdminDashboardPage.jsx
@@ -63,6 +63,7 @@ const AdminDashboardPage = () => {
     })
   }
 
+
   const handleLogout = () => {
     dispatch({type: "LOGOUT"})
     navigate("/admin/login")
@@ -71,25 +72,28 @@ const AdminDashboardPage = () => {
     <>
       <div className="w-full px-[5rem] pt-3 overflow-y-scroll h-screen bg-[#111111]">
 
-        <div className="flex items-center justify-between mb-[4rem]">
+        <div className="flex items-center justify-between mb-[4rem] ">
           <h1 className="text-[3rem] text-white font-bold">App</h1>
-          <button onClick={handleLogout} className="text-white text-md flex">
-            <img src={logout} alt="logout icon" />
+          <button onClick={handleLogout} className="text-white text-md bg-[#9BFF00] px-4 py-2 text-[#050505] rounded-[20px] flex">
+            <img className="mr-1" src={logout} alt="logout icon" />
             logout
           </button>
         </div>
 
 
-      <div className="flex justify-between text-[#FFFFFF] opacity-[0.5">
-          <h1 className="text-[2rem] text-[#FFFFFF]">Today's Leaderboard</h1>
-          <span className="text-white flex items-center bg-[#1D1D1D] rounded-[13px] px-5 text-sm">
+      <div className="flex justify-between">
+          <h1 className="text-[2rem] text-[#666666]">Today's Leaderboard</h1>
+          <span className="text-[#666666] flex items-center bg-[#1D1D1D] rounded-[13px] px-5 text-sm">
             <span>30 may 2022</span>
+            <span className=" ml-2 mb-2 text-[#666666]">.</span>
             <span className="rounded-[5px] bg-[#9BFF00] px-[.1rem] text-[12px] text-black mx-[.7rem]">SUBMISSIONS OPEN</span>
+            <span className=" mr-2 mb-2 text-[#666666]">.</span>
+            
             <span>11:34</span>
           </span>
       </div>
 
-      <div className="flex justify-between text-[#FFFFFF] mt-9 mb-[2.4rem] opacity-[0.5]">
+      <div className="flex justify-between px-3 text-[#666666] mt-9 mb-[2.4rem] ">
         <div className="flex">
           <h4 className="mr-5">#</h4>
           <h4>Title</h4>
@@ -111,10 +115,10 @@ const AdminDashboardPage = () => {
       }
       </div>
       <div className="flex justify-center text-md py-8">
-        <button onClick={handlePrev} className="text-[1.2rem] bg-[white] bo w-[150px] rounded-[12px] px-5 mr-[1rem] bg-gray">prev</button>
+        <button onClick={handlePrev} className="text-[1.2rem] bg-[white] bo w-[100px] rounded-[12px] px-5 mr-[1rem] bg-gray">prev</button>
         <button 
           onClick={handleNext} 
-        className=" bg-[white] text-[1.2rem] w-[150px] rounded-[12px] px-5 bg-gray">next</button>
+        className=" bg-[white] text-[1.2rem] ml-5 w-[100px] rounded-[12px] px-5 bg-gray">next</button>
       </div>
 
 
